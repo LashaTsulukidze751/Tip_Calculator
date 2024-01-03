e51d7e64f9043d14655349ec291d280f357a3feb
Lasha <babaxatsulukidze@gmail.com>
2024-01-03 20:05:11 +0400

    feat: finished project

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="./favicon.png" sizes="any" />
+      </head>
       <body className={SpaceMono.className}>{children}</body>
     </html>
   );
